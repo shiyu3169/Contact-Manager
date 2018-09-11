e2961f560a226f5314bed9c16b0e8e13bbde6790
Shiyu Wang <shiyu3169@gmail.com>
2018-09-11 16:15:06 -0400

    destrucuring

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Header = props => {
+  const { branding } = props;
   return (
     <div>
-      <h1>{props.branding}</h1>
+      <h1>{branding}</h1>
     </div>
   );
 };
