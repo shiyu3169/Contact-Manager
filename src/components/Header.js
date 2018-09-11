2acfe4560a0b085686f56d61217d11f4ddea66da
Shiyu Wang <shiyu3169@gmail.com>
2018-09-11 16:13:15 -0400

    working with props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Header = () => {
+const Header = props => {
   return (
     <div>
-      <h1>Contact manager</h1>
+      <h1>{props.branding}</h1>
     </div>
   );
 };
