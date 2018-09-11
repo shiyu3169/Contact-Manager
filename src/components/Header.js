fb0b6e132bbe584ed5f27eb7dd7d4d977e9d1ec5
Shiyu Wang <shiyu3169@gmail.com>
2018-09-11 16:20:34 -0400

    default props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Header = props => {
   const { branding } = props;
+
   return (
     <div>
       <h1>{branding}</h1>
     </div>
   );
+};
+
+Header.defaultProps = {
+  branding: "My App"
 };
 
 export default Header;
