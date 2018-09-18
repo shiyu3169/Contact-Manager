1a4a6bf6e9f8bb6b1851d955db314540b3d6b24b
Shiyu Wang <shiyu3169@gmail.com>
2018-09-18 15:31:23 -0400

    bootstrap and style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
-import "./App.css";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smite"
-          email="karen@gmail.com"
-          phone="555-555-5555"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smite"
+            email="karen@gmail.com"
+            phone="555-555-5555"
+          />
+        </div>
       </div>
     );
   }
