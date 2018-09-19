9b437065505c8b158b057780807cb0d6cc843501
Shiyu Wang <shiyu3169@gmail.com>
2018-09-19 14:02:40 -0400

    no need constructor

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,31 +2,29 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 export default class Contacts extends Component {
-    constructor() {
-        super();
-        this.state = {
-            contacts: [
-                {
-                    id: 1,
-                    name: "John Doe",
-                    email: "jdoe@gmail.com",
-                    phone: "555-555-5555"
-                },
-                {
-                    id: 2,
-                    name: "Karen Williams",
-                    email: "karen@gmail.com",
-                    phone: "222-222-2222"
-                },
-                {
-                    id: 3,
-                    name: "Henry Jognson",
-                    email: "henry@gmail.com",
-                    phone: "111-111-1111"
-                }
-            ]
-        };
-    }
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: "John Doe",
+                email: "jdoe@gmail.com",
+                phone: "555-555-5555"
+            },
+            {
+                id: 2,
+                name: "Karen Williams",
+                email: "karen@gmail.com",
+                phone: "222-222-2222"
+            },
+            {
+                id: 3,
+                name: "Henry Jognson",
+                email: "henry@gmail.com",
+                phone: "111-111-1111"
+            }
+        ]
+    };
+
     render() {
         const { contacts } = this.state;
         return (
