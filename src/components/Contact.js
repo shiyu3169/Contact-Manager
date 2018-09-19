887344724dd09f8b9e934e1ec97b8eb56e9b0cc6
Shiyu Wang <shiyu3169@gmail.com>
2018-09-19 15:19:12 -0400

    react.fragment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,11 @@ import PropTypes from "prop-types";
 
 export default class Contact extends Component {
     static propTypes = {
-        name: PropTypes.string.isRequired,
-        email: PropTypes.string.isRequired,
-        phone: PropTypes.string.isRequired
+        contact: PropTypes.object.isRequired
+    };
+
+    onShowClick = e => {
+        console.log(e.target);
     };
 
     render() {
@@ -13,7 +15,13 @@ export default class Contact extends Component {
 
         return (
             <div className="card card-body mb-3">
-                <h4>{name}</h4>
+                <h4>
+                    {name}{" "}
+                    <i
+                        onClick={this.onShowClick}
+                        className="fas fa-sort-down"
+                    />
+                </h4>
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
