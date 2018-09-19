a3d6cfa15faf3398b64f018d60fbdf1f5186e6b0
Shiyu Wang <shiyu3169@gmail.com>
2018-09-19 15:31:56 -0400

    show contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,26 +6,33 @@ export default class Contact extends Component {
         contact: PropTypes.object.isRequired
     };
 
-    onShowClick = e => {
-        console.log(e.target);
+    state = {
+        showContactInfo: true
     };
 
     render() {
         const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
 
         return (
             <div className="card card-body mb-3">
                 <h4>
                     {name}{" "}
                     <i
-                        onClick={this.onShowClick}
+                        onClick={() => {
+                            this.setState({
+                                showContactInfo: !showContactInfo
+                            });
+                        }}
                         className="fas fa-sort-down"
                     />
                 </h4>
-                <ul className="list-group">
-                    <li className="list-group-item">Email: {email}</li>
-                    <li className="list-group-item">Phone: {phone}</li>
-                </ul>
+                {showContactInfo ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">Email: {email}</li>
+                        <li className="list-group-item">Phone: {phone}</li>
+                    </ul>
+                ) : null}
             </div>
         );
     }
