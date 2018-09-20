bdd859737db74ee9087d2679cda86357e4c7ca04
Shiyu Wang <shiyu3169@gmail.com>
2018-09-20 14:17:33 -0400

    dispatch

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,65 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Consumer } from "../context";
 
 export default class Contact extends Component {
     static propTypes = {
-        contact: PropTypes.object.isRequired,
-        deleteClickhandler: PropTypes.func.isRequired
+        contact: PropTypes.object.isRequired
     };
 
     state = {
         showContactInfo: false
     };
 
-    onDeleteClick = () => {
-        this.props.deleteClickhandler();
+    onDeleteClick = (id, dispatch) => {
+        dispatch({ type: "DELETE_CONTACT", payload: id });
     };
 
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { id, name, email, phone } = this.props.contact;
         const { showContactInfo } = this.state;
 
         return (
-            <div className="card card-body mb-3">
-                <h4>
-                    {name}{" "}
-                    <i
-                        onClick={() => {
-                            this.setState({
-                                showContactInfo: !showContactInfo
-                            });
-                        }}
-                        className="fas fa-sort-down"
-                        style={{ cursor: "pointer" }}
-                    />
-                    <i
-                        className="fas fa-times float-right"
-                        style={{ cursor: "pointer", color: "red" }}
-                        onClick={this.onDeleteClick}
-                    />
-                </h4>
-                {showContactInfo ? (
-                    <ul className="list-group">
-                        <li className="list-group-item">Email: {email}</li>
-                        <li className="list-group-item">Phone: {phone}</li>
-                    </ul>
-                ) : null}
-            </div>
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div className="card card-body mb-3">
+                            <h4>
+                                {name}{" "}
+                                <i
+                                    onClick={() => {
+                                        this.setState({
+                                            showContactInfo: !showContactInfo
+                                        });
+                                    }}
+                                    className="fas fa-sort-down"
+                                    style={{ cursor: "pointer" }}
+                                />
+                                <i
+                                    className="fas fa-times float-right"
+                                    style={{ cursor: "pointer", color: "red" }}
+                                    onClick={this.onDeleteClick.bind(
+                                        this,
+                                        id,
+                                        dispatch
+                                    )}
+                                />
+                            </h4>
+                            {showContactInfo ? (
+                                <ul className="list-group">
+                                    <li className="list-group-item">
+                                        Email: {email}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Phone: {phone}
+                                    </li>
+                                </ul>
+                            ) : null}
+                        </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
