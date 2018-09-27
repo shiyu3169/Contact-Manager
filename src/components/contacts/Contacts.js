4892acc52f71f75e918295f6f600b5a4c5f7ad8a
Shiyu Wang <shiyu3169@gmail.com>
2018-09-27 16:17:33 -0400

    move action to another file

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { GET_CONTACTS } from "../../actions/types";
+import { getContacts } from "../../actions/contactActions";
 
 class Contacts extends Component {
     componentDidMount() {
@@ -11,7 +11,6 @@ class Contacts extends Component {
 
     render() {
         const { contacts } = this.props;
-        console.log(contacts);
         return (
             <React.Fragment>
                 <h1 className="display-4 mb-2">
@@ -34,11 +33,7 @@ const mapStateToProps = state => ({
     contacts: state.contact.contacts
 });
 
-const mapDispatchToProps = dispatch => ({
-    getContacts: () => dispatch({ type: GET_CONTACTS })
-});
-
 export default connect(
     mapStateToProps,
-    mapDispatchToProps
+    { getContacts }
 )(Contacts);
