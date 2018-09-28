9dc41c35f2b3b882901a9c46e1463071590a17f9
Shiyu Wang <shiyu3169@gmail.com>
2018-09-28 11:05:27 -0400

    delete request dispatch

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -9,11 +9,12 @@ export const getContacts = () => async dispatch => {
     });
 };
 
-export const deleteContact = id => {
-    return {
+export const deleteContact = id => async dispatch => {
+    await Axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({
         type: DELETE_CONTACT,
         payload: id
-    };
+    });
 };
 
 export const addContact = contact => {
