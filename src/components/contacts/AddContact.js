aaca986d53c716770cd3f8630ea10d136e495c20
Shiyu Wang <shiyu3169@gmail.com>
2018-09-28 10:51:20 -0400

    add contact

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import TextInputGroup from "../layout/TextInputGroup";
-import axios from "axios";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addContact } from "../../actions/contactActions";
+import uuid from "uuid";
 
-export default class AddContact extends Component {
+class AddContact extends Component {
     state = {
         name: "",
         email: "",
@@ -16,7 +19,7 @@ export default class AddContact extends Component {
         });
     };
 
-    onSubmit = async (dispatch, e) => {
+    onSubmit = e => {
         e.preventDefault();
         const { name, email, phone } = this.state;
 
@@ -43,17 +46,14 @@ export default class AddContact extends Component {
         }
 
         const newContact = {
+            id: uuid(),
             name,
             email,
             phone
         };
 
-        const res = await axios.post(
-            "https://jsonplaceholder.typicode.com/users",
-            newContact
-        );
-
-        dispatch({ type: "ADD_CONTACT", payload: res.data });
+        // Submit Contact
+        this.props.addContact(newContact);
 
         // Clear State
         this.setState({
@@ -70,58 +70,55 @@ export default class AddContact extends Component {
         const { name, email, phone, errors } = this.state;
 
         return (
-            <div>
-                {value => {
-                    const { dispatch } = value;
-                    return (
-                        <div className="card mb-3">
-                            <div className="card-header">Add Contact</div>
-                            <div className="card-body">
-                                <form
-                                    onSubmit={this.onSubmit.bind(
-                                        this,
-                                        dispatch
-                                    )}
-                                >
-                                    <TextInputGroup
-                                        label="Name"
-                                        placeholder="Enter Name..."
-                                        value={name}
-                                        onChange={this.onChange}
-                                        name="name"
-                                        id="name"
-                                        error={errors.name}
-                                    />
-                                    <TextInputGroup
-                                        label="Email"
-                                        placeholder="Enter Email..."
-                                        value={email}
-                                        onChange={this.onChange}
-                                        name="email"
-                                        id="email"
-                                        type="email"
-                                        error={errors.email}
-                                    />
-                                    <TextInputGroup
-                                        label="Phone"
-                                        placeholder="Enter Phone..."
-                                        value={phone}
-                                        onChange={this.onChange}
-                                        name="phone"
-                                        id="phone"
-                                        error={errors.phone}
-                                    />
-                                    <input
-                                        type="submit"
-                                        value="Add Contact"
-                                        className="btn btn-light btn-block"
-                                    />
-                                </form>
-                            </div>
-                        </div>
-                    );
-                }}
+            <div className="card mb-3">
+                <div className="card-header">Add Contact</div>
+                <div className="card-body">
+                    <form onSubmit={this.onSubmit.bind(this)}>
+                        <TextInputGroup
+                            label="Name"
+                            placeholder="Enter Name..."
+                            value={name}
+                            onChange={this.onChange}
+                            name="name"
+                            id="name"
+                            error={errors.name}
+                        />
+                        <TextInputGroup
+                            label="Email"
+                            placeholder="Enter Email..."
+                            value={email}
+                            onChange={this.onChange}
+                            name="email"
+                            id="email"
+                            type="email"
+                            error={errors.email}
+                        />
+                        <TextInputGroup
+                            label="Phone"
+                            placeholder="Enter Phone..."
+                            value={phone}
+                            onChange={this.onChange}
+                            name="phone"
+                            id="phone"
+                            error={errors.phone}
+                        />
+                        <input
+                            type="submit"
+                            value="Add Contact"
+                            className="btn btn-light btn-block"
+                        />
+                    </form>
+                </div>
             </div>
         );
     }
 }
+
+AddContact.propTypes = {
+    addContact: PropTypes.func.isRequired
+};
+
+export default connect(
+    null,
+    { addContact }
+)(AddContact);
